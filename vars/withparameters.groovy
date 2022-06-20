20e247953437562b308b63fa551a5726e4b1bbfd
shaikkarim196 <105496453+shaikkarim196@users.noreply.github.com>
2022-06-20 10:13:08 +0530

    Create withparameters.groovy

---
+++ vars/withparameters.groovy
@@ -0,0 +1,12 @@
+def call(Integer a = 15, Integer b = 3) {
+  echo $a + "---" + $b
+
+echo "a =  ${a}"
+echo "b = ${b}"
+echo "a + b = ${a + b}"
+echo "a - b = ${a - b}"
+echo "a * b = ${a * b}"
+echo "a / b = ${a / b}"
+echo "a % b = ${a % b}"
+echo "a ** b = ${a ** b}"
+}
